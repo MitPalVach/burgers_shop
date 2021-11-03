5fce6293c7698b717362d5ef5255ee2c08e32ca5
MitPalVach <73882508+MitPalVach@users.noreply.github.com>
2021-11-03 18:20:02 +0300

    create cartItem component;

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ export {default as Categories} from './Categories/Categories'
 export {default as SortPopup} from './SortPopup/SortPopup'
 export {default as BurgerBlock} from './BurgerBlock/BurgerBlock'
 export {default as BurgersLoading} from './BurgerBlock/BurgersLoading'
+export {default as CartItem} from './CartItem/CartItem'
 
 
 
